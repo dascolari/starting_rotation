3f5436929f1263c47076bf7c76bd88df5bf59750
harrison-snell <hsnell@utexas.edu>
2022-04-17 18:51:41 -0500

    cluster

---
+++ starting_rotation/code/baseball_script.R
@@ -0,0 +1,262 @@
+library(caret)
+library(tidyverse)
+library(rpart)
+library(rpart.plot)
+library(mosaic)
+library(rsample)
+library(modelr)
+library(randomForest)
+library(gbm)
+library(foreach)
+library(pitchRx)
+
+pitch_ab_2015 <- read.csv("~/School/University of Texas-Austin/Classes/Data Mining/pitch_ab_2015.csv")
+
+
+test_arrieta = pitch_ab_2015 %>%
+  filter(pitcher_id == 453562)
+
+
+test_arrieta <- test_arrieta %>% 
+  mutate(first_pitch = ifelse(pitch_num == 1, 1, 0), 
+         prev_pitch = ifelse(first_pitch == 0, lag(pitch_type), "none")) %>%
+  filter(pitch_type != "")%>%
+  select(pitch_type, b_score, b_count, s_count, outs, pitch_num, on_1b, on_2b, on_3b, p_score, p_throws, stand, prev_pitch, first_pitch, px, pz)
+
+test_arrieta$pitch_type = factor(test_arrieta$pitch_type)
+test_arrieta$prev_pitch = factor(test_arrieta$prev_pitch)
+# test_arrieta$stand = factor(test_arrieta$stand)
+
+test_split = initial_split(test_arrieta, 0.8)
+test_train = training(test_split)
+test_test = testing(test_split)
+
+# prune_1se = function(my_tree) {
+#   out = as.data.frame(my_tree$cptable)
+#   thresh = min(out$xerror + out$xstd)
+#   cp_opt = max(out$CP[out$xerror <= thresh])
+#   prune(my_tree, cp=cp_opt)
+# }
+
+
+# Predict first pitch with overall career metrics and batter metrics
+# Then we can use lag pitches for later pitches
+
+# bball_cart_folds = crossv_kfold(test, k=5)
+
+# cart_models = map(bball_cart_folds$train, ~prune_1se(rpart(pitch_type ~ prev_pitch,
+#                                                         data =., 
+#                                                         control = rpart.control(cp = .0001, minsplit = 10))))
+# cart_errs = map2_dbl(cart_models, ca_cart_folds$test, modelr::rmse)
+# cart_errs = mean(cart_errs)
+
+# first_try = rpart(pz ~ pitch_type,
+#                        data =test_train, 
+#                        control = rpart.control(cp = .0001, minsplit = 10))
+
+# rpart.plot(prune_1se(first_try), type=4, extra=1)
+
+# rmse(prune_1se(first_try), test_test)
+
+pitch_count = test_test %>%
+  group_by(pitch_type)%>%
+  summarize(count = n())
+
+
+
+first_try = randomForest(pitch_type ~ .-px-pz,
+                  data =test_train, 
+                  importance = TRUE)
+
+
+test_train = test_train %>%
+  mutate(pred_pitch = predict(first_try, test_train))
+
+
+px_model = randomForest(px~.-pz-pitch_type,
+                        data=test_train,
+                        importance = TRUE)
+
+test_train = test_train %>%
+  mutate(pred_px = predict(px_model, test_train))
+
+pred_pitch = as.data.frame(predict(first_try, test_train))
+pred_pitch = pred_pitch %>%
+  mutate(actual = test_train$pitch_type)
+
+pz_model = randomForest(pz~.-px-pitch_type,
+                        data = test_train,
+                        importance = TRUE)
+
+test_train = test_train %>%
+  mutate(pred_pz = predict(pz_model, test_train))
+
+test_test = test_test %>%
+  mutate(pred_pitch = predict(first_try, test_test))
+
+
+test_test = test_test %>%
+  mutate(pred_px = predict(px_model, test_test))
+
+test_test = test_test %>%
+  mutate(pred_pz = predict(pz_model, test_test))
+
+
+
+# Make a table of classification errors
+xtabs(~pitch_type + pred_pitch, data=test_test)
+
+# Performance
+nrow(test_test)
+(xtabs(~pitch_type + pred_pitch, data=test_test) %>% diag %>% sum)/nrow(test_test)
+
+# q_hat = as.data.frame(predict(second_try, test_test))
+# y_hat = as.data.frame(y_hat)
+# q_hat = as.data.frame(y_hat)
+# yhat_test = as.data.frame(ifelse(y_hat == pmax(y_hat$CU, y_hat$CH,
+#                                  y_hat$IN, y_hat$FF,
+#                                  y_hat$SI, y_hat$SL), 1, 0))
+
+# pred_pitch_count = y_hat %>%
+#   group_by(y_hat)%>%
+#   summarize(count = n())
+
+
+# sum(yhat_test$CU)
+
+# tab <- tibble::as_tibble(yhat_test)
+
+# transform to long format the dummy columns
+# tab_long <- tidyr::pivot_longer(tab, 
+#                                cols = tidyselect::contains("."),
+#                                 names_to = c("groups", "levels"),
+#                                 names_pattern = "^([^.]*)[.](.*)")
+
+
+
+
+# groups <- unique(tab_long$groups)
+# keep only non dummy value and do not keep temp value col
+# tab_filter <- dplyr::select(
+#  dplyr::filter(tab_long, value == 1),
+#  -value)
+# tranform to wide format   
+# tab_wide <- tidyr::pivot_wider(
+#   tab_filter,
+#   names_from = groups, 
+#   values_from = levels)
+# convert to factors the groups column
+# new_tab <- dplyr::mutate_at(
+#  tab_wide,
+#   groups,
+#   ~ forcats::as_factor(.)
+# )
+# dplyr::glimpse(new_tab)
+
+# sum(yhat_test$CU)
+
+
+# Player Search
+
+ide = ifelse(player_names$first_name == "Chad" & 
+               player_names$last_name =="Bettis",
+             player_names$id,0)
+sum(ide)
+
+name_list = c(453562,506433,519455,518516,519242,446372,456034,
+              544931,453286,433587,477132,594798,502042,425844,
+              453343,572096,547973,502202,434718,572971,474521,
+              502154,543521,543243,451584,453192,502381,543037,
+              608379,518452)
+
+starters = filter(player_names, id %in% name_list)
+
+write.csv(starters, "first_lineup.csv", row.names = FALSE)
+
+working = pitch_ab_2015 %>%
+  mutate(first_pitch = ifelse(pitch_num == 1, 1, 0), 
+       prev_pitch = ifelse(first_pitch == 0, lag(pitch_type), "none")) %>%
+  filter(pitch_type != "" | pitch_type != " ")%>%
+  select(pitcher_id, pitch_type, b_score, b_count, s_count, outs, pitch_num, on_1b, on_2b, on_3b, p_score, p_throws, stand, prev_pitch, first_pitch, px, pz)
+
+working$pitch_type = factor(working$pitch_type)
+working$prev_pitch = factor(working$prev_pitch)
+
+for (i in 1:30) {
+  what = as.numeric(starters$id[i])
+  test = working %>%
+    filter(pitcher_id == what)
+}
+
+
+#swing_at_random = foreach(i = starters, .combine='rbind') %dopar% {
+  working <- working %>% 
+    filter(pitcher_id == starters$id[15])
+  
+  #pitches_pitcher_2015$pitch_type <- na.omit(pitches_pitcher_2015$pitch_type)
+  
+  #pitches_pitcher_2015$pitch_type <- as.factor(pitches_pitcher_2015$pitch_type)
+  
+  ## Train / Test
+  
+  pitcher_split =  initial_split(working, prop=0.8)
+  pitcher_train = training(pitcher_split)
+  pitcher_test  = testing(pitcher_split)
+  
+  ## Random Forest
+  
+  pitch.forest = randomForest(pitch_type ~ ., 
+                              data=pitcher_train, na.action=na.omit)
+  
+  pitcher_test = pitcher_test %>% 
+    mutate(pitch_pred = predict(pitch.forest, pitcher_test, type='class'))
+  
+  # Make a table of classification errors
+  xtabs(~pitch_type + pitch_pred, data=pitcher_test)
+  
+  # Performance
+  nrow(pitcher_test)
+  hit_rate = (xtabs(~pitch_type + pitch_pred, data=pitcher_test) %>% diag %>% sum)/nrow(pitcher_test)
+  
+#  c(id = i, rforest = hit_rate)
+#} %>% as.data.frame
+
+performance = merge(x = swing_at_random, y = swing_naively)
+performance = merge(x = performance, y = player_names)
+
+
+
+#### Analyzing Different pitches
+
+pitches_2015 <- read.csv("~/School/University of Texas-Austin/Classes/Data Mining/pitches_2015.csv")
+
+
+
+test = pitch_ab_2015 %>%
+  filter(pitcher_id == 453562)%>%
+  filter(pitch_type != "") %>%
+  filter(pitch_type != "IN")
+  select(pitch_type,px,pz,start_speed,end_speed,spin_rate,spin_dir,break_angle,break_length,break_y)
+
+test$pitch_type = as.factor(test$pitch_type)
+
+ggplot(test)+
+  geom_point(aes(x=break_angle,y=break_length,color=pitch_type, size=break_y),alpha=.2)
+
+ggplot(test)+
+  geom_point(aes(x=px, y=pz))+
+  xlim(-4,4)
+
+
+X = test[,-(1:7)]
+X = scale(X, center=TRUE, scale=TRUE)
+
+# Extract the centers and scales from the rescaled data (which are named attributes)
+mu = attr(X,"scaled:center")
+sigma = attr(X,"scaled:scale")
+
+# K-means++
+clust_kplus = kmeanspp(X, k=6, nstart=25)
+
+ggplot(test)+
+  geom_point(aes(x=break_angle,y=break_length,color=clust_kplus$cluster),alpha=.2)
