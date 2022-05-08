4c0ea50bc3c02c1bfeb4bb947aa56a208105e40c
bjwilliams5 <98039212+bjwilliams5@users.noreply.github.com>
2022-05-08 18:00:20 -0500

    Update situation.R

--- starting_rotation/code/situation.R
+++ starting_rotation/code/situation.R
@@ -1,33 +1,45 @@
+# Enter the game situation 
+
+id = 12
+stance = "R"
+strikes = 1
+balls = 1
+out = 1
+
+# Call the pitcher's dataset 
+
 load(file.path(path, "output", "pitches_import.RData"))
 
-id = 13
-stance = "R"
-strikes = 0
-balls = 0
-outs = 0
+fname_model <- paste0("trashcans", id, ".RData")
+load(file = file.path(path, 'output', 'models', fname_model))
 
+fname_pitcher <- paste0("pitcher", id, ".Rds")
+load(file = file.path(path, 'output', 'pitchers', fname_pitcher))
 
-  fname_model <- paste0("trashcans", id, ".RData")
-  load(file = file.path(path, 'output', 'models', fname_model))
-  
-  fname_pitcher <- paste0("pitcher", id, ".Rds")
-  load(file = file.path(path, 'output', 'pitchers', fname_pitcher))
-  
-  fname_factor <- paste0("pitcher_factor", id, ".RDs")
-  load(file = file.path(path, 'output', 'pitchers', fname_factor))
-  
-  pitcher_first <- names$first_name[id]
-  pitcher_last <- names$last_name[id]
+fname_factor <- paste0("pitcher_factor", id, ".RDs")
+load(file = file.path(path, 'output', 'pitchers', fname_factor))
 
-  # out of sample performance (osp)
-  # re-factor to get most thrown pitch first in each table
-  pitcher$pitch_type <- factor(pitcher$pitch_type, factor_types)
-  pitcher_predi <- pitcher %>% 
-    mutate(pitchhat_trashcan = predict(trashcan_1, pitcher))
-  pitcher_predi$pitchhat_trashcan = factor(pitcher_predi$pitchhat_trashcan, factor_types)
-  
+pitcher_first <- names$first_name[id]
+pitcher_last <- names$last_name[id]
+
+# affix the model prediction results to the pitches dataset
+
+pitcher$pitch_type <- factor(pitcher$pitch_type, factor_types)
+pitcher_predi <- pitcher %>% 
+  mutate(pitchhat_trashcan = predict(trashcan_3, pitcher))
+pitcher_predi$pitchhat_trashcan = factor(pitcher_predi$pitchhat_trashcan, factor_types)
+
+# filter by game situation
+
   pitcher_result <- pitcher_predi %>% 
-    filter(stand == stance, s_count == strikes, b_count == balls, outs == outs)
+    filter(
+      stand == stance &          
+        s_count == strikes &
+           b_count == balls &
+           outs == out
+           )
+
+# summarize the predictions and the actual results 
   
   pitcher_guess <- pitcher_result %>% 
     group_by(pitchhat_trashcan) %>% 
@@ -39,15 +51,10 @@ outs = 0
     group_by(pitch_type) %>% 
     summarize(actual = n()) %>% 
     mutate(actual_percent = actual / sum(actual))
-  
-  pitcher_result <- pitcher_result %>% 
-    mutate(success_trashcan = ifelse(pitch_type == pitchhat_trashcan, 1, 0)) 
-  
-#  pitcher_confidence <-  pitcher_result %>% 
-#    group_by(pitch_type) %>% 
-#    summarize(confidence = sum(success_trashcan) / n())
-  
+
   situation <- merge(pitcher_guess, pitcher_real)
-#  situation <- merge(situation, pitcher_confidence)
+
+  situation <-situation %>% 
+    arrange(desc(percent))
   
-  
+situation  
