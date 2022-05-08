ebac85e84fec9390cf6277849af69d08c0c2a947
bjwilliams5 <98039212+bjwilliams5@users.noreply.github.com>
2022-05-08 18:47:52 -0500

    cleaning old files and archiving

--- starting_rotation/code/spring_training_brandon.R
+++
@@ -1,4 +0,0 @@
-# spring training tryouts - brandon
-pitches <- read.csv(file.path(path, 'data', 'raw', 'pitches', 'pitches.csv'))
-atbats <- read.csv(file.path(path, 'data', 'raw', 'archive', 'atbats.csv'))
-pitches <- merge(pitches, atbats) 
